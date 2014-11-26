42c697ef9ad79971759ec84bbf13465fb51095bd
fen <fojuth@gmail.com>
2014-11-26 22:49:43 +0100

    FormatInterface comments.

--- src/Output/Format/FormatInterface.php
+++ src/Output/Format/FormatInterface.php
@@ -6,10 +6,25 @@ interface FormatInterface {
 
     public function setIssueTrackerUrlPattern($pattern);
 
+    /**
+     * Decorates the output (e.g. adds linkgs to the issue tracker)
+     *
+     * @return self
+     */
     public function decorate();
 
+    /**
+     * Returns a write-ready log.
+     *
+     * @return array
+     */
     public function generate();
 
+    /**
+     * Returns the output filename.
+     *
+     * @return string
+     */
     public function getFileName();
 
 } 
